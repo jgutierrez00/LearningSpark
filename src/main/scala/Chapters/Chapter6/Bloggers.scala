af6084de2b385553741711cc79dfd6feb84e3693
jgutierrez00 <sejogutierrez00@gmail.com>
2025-02-25 13:18:08 +0100

    Added Chapter 6 case classes

---
+++ src/main/scala/Chapters/Chapter6/Bloggers.scala
@@ -0,0 +1,12 @@
+package Chapters.Chapter6
+
+case class Bloggers
+(
+  id: Int,
+  first: String,
+  last: String,
+  url: String,
+  date: String,
+  hits: Int,
+  campaigns: Array[String]
+)
---
+++ src/main/scala/Chapters/Chapter6/CaseClasses.scala
@@ -0,0 +1,10 @@
+package Chapters.Chapter6
+
+import Chapters.Chapter
+import org.apache.spark.sql._
+
+object CaseClasses extends Chapter{
+  override def run(spark: SparkSession, args: Array[String]): Unit = {
+
+  }
+}
