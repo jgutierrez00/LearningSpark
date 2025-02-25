a2616debe6e87c92963d632dabb991e24dcee866
Jose Maria Gutierrez <josemaria.gutierrez@bosonit.com>
2025-02-25 09:50:27 +0100

    Modificado lanzador de objetos por argumento

--- src/main/scala/Chapters/App.scala
+++ src/main/scala/Chapters/App.scala
@@ -7,7 +7,6 @@ import Chapters.Chapter4.{Avro, BasicQueries, Binaries, CSV, DataReader, DataWri
 import Chapters.Chapter5.{AzureCosmos, MSSQL, MySQL, PostgreSQL, UDFs}
 import org.apache.spark.sql.SparkSession
 
-
 object App {
 
   def main(args: Array[String]): Unit = {
@@ -22,57 +21,15 @@ object App {
     val spark: SparkSession = SparkSession.builder()
       .appName("LearningSpark")
       .master("local[*]")
-      .config("spark.jars.packages", "databricks:spark-deep-learning:1.5.0-spark2.4-s_2.11")
       .getOrCreate()
 
     try{
 
-      className match {
-        case "Chapter1.MnMcount" =>
-          MnMcount.run(spark, chapterArgs)
-        case "Chapter2.AuthorsAges" =>
-          AuthorsAges.run(spark)
-        case "Chapter3.Schemas" =>
-          Schemas.run(spark, chapterArgs)
-        case "Chapter3.Schemas2" =>
-          Schemas2.run(spark, chapterArgs)
-        case "Chapter4.BasicQueries" =>
-          BasicQueries.run(spark, chapterArgs)
-        case "Chapter4.Tables" =>
-          Tables.run(spark, chapterArgs)
-        case "Chapter4.Views" =>
-          Views.run(spark, chapterArgs)
-        case "Chapter4.MetaData" =>
-          MetaData.run(spark, chapterArgs)
-        case "Chapter4.DataReader" =>
-          DataReader.run(spark, chapterArgs)
-        case "Chapter4.DataWriter" =>
-          DataWriter.run(spark, chapterArgs)
-        case "Chapter4.Parquet" =>
-          Parquet.run(spark, chapterArgs)
-        case "Chapter4.JSON" =>
-          JSON.run(spark, chapterArgs)
-        case "Chapter4.CSV" =>
-          CSV.run(spark, chapterArgs)
-        case "Chapter4.Avro" =>
-          Avro.run(spark, chapterArgs)
-        case "Chapter4.ORC" =>
-          ORC.run(spark, chapterArgs)
-        case "Chapter4.Images" =>
-          Images.run(spark, chapterArgs)
-        case "Chapter4.Binaries" =>
-          Binaries.run(spark, chapterArgs)
-        case "Chapter5.UDFs" =>
-          UDFs.run(spark)
-        case "Chapter5.PostgreSQL" =>
-          PostgreSQL.run(spark, chapterArgs)
-        case "Chapter5.MySQL" =>
-          MySQL.run(spark, chapterArgs)
-        case "Chapter5.AzureCosmos" =>
-          AzureCosmos.run(spark, chapterArgs)
-        case "Chapter5.MSSQL" =>
-          MSSQL.run(spark, chapterArgs)
-      }
+      val clazz = Class.forName("Chapters."+className + "$")
+      val module = clazz.getField("MODULE$").get(null)
+      val runMethod = clazz.getMethod("run", classOf[SparkSession], classOf[Array[String]])
+      runMethod.invoke(module, spark, chapterArgs)
+
     }catch{
       case ex: ClassNotFoundException =>
         println(s"Error: La clase ${className} no existe en el paquete Chapters")
@@ -80,7 +37,7 @@ object App {
 
       case unknowErr: Exception =>
         println(s"Error al ejecutar el job: ${unknowErr.getMessage}")
-        println(unknowErr.toString())
+        println(unknowErr.printStackTrace())
     }finally{
       spark.stop()
     }
