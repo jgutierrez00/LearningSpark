820ca58437c39b5416a5c227faa270237267db3a
jgutierrez00 <sejogutierrez00@gmail.com>
2025-02-26 16:48:53 +0100

    Added StreamingQuery exercise

---
+++ src/main/scala/Chapters/Chapter8/StreamingQuery.scala
@@ -0,0 +1,29 @@
+package Chapters.Chapter8
+
+import Chapters.Chapter
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming._
+
+object StreamingQuery extends Chapter{
+  override def run(spark: SparkSession, args: Array[String]): Unit = {
+
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
+
+    val words = lines.select(split(col("value"), "\\s").as("word"))
+    val counts = words.groupBy("word").count()
+
+    val streamingQuery = counts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .trigger(Trigger.ProcessingTime("1 second"))
+      .option("checkpointLocation", "/tmp/checkpoint")
+      .start()
+
+    streamingQuery.awaitTermination()
+  }
+}
