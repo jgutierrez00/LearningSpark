b01c10d9b6613c41448e464bff5a11c203c610ad
jgutierrez00 <sejogutierrez00@gmail.com>
2025-02-26 17:13:53 +0100

    Added query listener

--- src/main/scala/Chapters/Chapter8/StreamingQuery.scala
+++ src/main/scala/Chapters/Chapter8/StreamingQuery.scala
@@ -8,14 +8,31 @@ import org.apache.spark.sql.streaming._
 object StreamingQuery extends Chapter{
   override def run(spark: SparkSession, args: Array[String]): Unit = {
 
+    val listener = new StreamingQueryListener() {
+      override def onQueryStarted(event: StreamingQueryListener.QueryStartedEvent): Unit = {
+        println("Query started: " + event.id)
+      }
+
+      override def onQueryTerminated(event: StreamingQueryListener.QueryTerminatedEvent): Unit = {
+        println("Query terminated: " + event.id)
+      }
+
+      override def onQueryProgress(event: StreamingQueryListener.QueryProgressEvent): Unit = {
+        println("Query made progress: " + event.progress)
+      }
+    }
+
+    spark.streams.addListener(listener)
+
     val lines = spark.readStream
       .format("socket")
       .option("host", "localhost")
       .option("port", 9999)
       .load()
 
-    val filteredLines = lines.filter("isCorruptedUdf(value) = false")
-    val words = filteredLines.select(split(col("value"), "\\s").as("word"))
+//    val filteredLines = lines.filter("isCorruptedUdf(value) = false")
+//    val words = filteredLines.select(split(col("value"), "\\s").as("word"))
+    val words = lines.select(split(col("value"), "\\s").as("word"))
     val counts = words.groupBy("word").count()
 
     val streamingQuery = counts.writeStream
