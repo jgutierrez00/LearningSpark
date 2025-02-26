53e93348151e2e4454cd3983a5227319fe49d85f
jgutierrez00 <sejogutierrez00@gmail.com>
2025-02-26 12:14:03 +0100

    Added exercise Persisting

---
+++ src/main/scala/Chapters/Chapter7/Persisting.scala
@@ -0,0 +1,25 @@
+package Chapters.Chapter7
+
+import Chapters.Chapter
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.storage.StorageLevel
+
+object Persisting extends Chapter{
+  override def run(spark: SparkSession, args: Array[String]): Unit = {
+
+    import spark.implicits._
+
+    val df = spark.range(1 * 10000000).toDF("id").withColumn("square", $"id" * $"id")
+    df.persist(StorageLevel.DISK_ONLY)
+
+    var startTime = System.nanoTime()
+    df.count()
+    var endTime = System.nanoTime()
+    println(s"First count took ${(endTime - startTime) / 1000000} miliseconds")
+
+    startTime = System.nanoTime()
+    df.count()
+    endTime = System.nanoTime()
+    println(s"Second count took ${(endTime - startTime) / 1000000} miliseconds")
+  }
+}
