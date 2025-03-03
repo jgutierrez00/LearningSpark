40aebb3036cb0e847b85dff5b454f747a8875304
jgutierrez00 <sejogutierrez00@gmail.com>
2025-03-03 17:12:10 +0100

    Added comments for window

--- src/main/scala/Chapters/Chapter5/Windowing.scala
+++ src/main/scala/Chapters/Chapter5/Windowing.scala
@@ -18,6 +18,16 @@ object Windowing extends Chapter{
 
     dataDS.show()
 
+    /*
+    No introducimos una particion porque en la imagen resultado no se aprecia ninguna. La primera apreciacion que se
+    ve es un orden por la columna power de forma descendente.
+    Si usamos esta condicion como Window y aplicamos las funciones correspondientes, obtenemos el resultado.
+
+    Usando una particion, obtendriamos esos resultados pero solo para cada particion, por ejemplo si usamos como particion
+    la columna "empresa", el rank, dense_rank y row_number se harian en sobre cada subgrupo de empresas.
+
+    Si usamos "name" entonces nos sale todo los resultados a 1 ya que no hay nombres iguales.
+     */
     val windowSpec = Window.orderBy(desc("power"))
 
     var modifiedDS = dataDS.withColumn("Rank", rank().over(windowSpec))
