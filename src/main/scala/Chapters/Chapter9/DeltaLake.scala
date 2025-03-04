adac204299f2abc48200a2f99ee3941834d24f48
jgutierrez00 <sejogutierrez00@gmail.com>
2025-03-04 12:25:01 +0100

    Added snapshots and auditing

--- src/main/scala/Chapters/Chapter9/DeltaLake.scala
+++ src/main/scala/Chapters/Chapter9/DeltaLake.scala
@@ -29,40 +29,61 @@ object DeltaLake extends Chapter{
 
     spark.sql("SELECT * FROM loans_delta").show()
 
-    import spark.implicits._
-
-    // Se pueden mergear esquemas en caso de que sean diferentes
-    val loanUpdates = Seq((1111111L, 1000, 1000.0, "TX", false), (2222222L, 2000, 0.0, "CA", true))
-      .toDF("loan_id", "funded_amnt", "paid_amnt", "addr_state", "closed")
-
-    loanUpdates.write
-      .format("delta")
-      .mode("append")
-      .option("mergeSchema", "true")
-      .save(deltaPath)
-
-    // Se pueden realizar operaciones SQL directamente
-
+//    import spark.implicits._
+//
+//    // Se pueden mergear esquemas en caso de que sean diferentes
+//    val loanUpdates = Seq((1111111L, 1000, 1000.0, "TX", false), (2222222L, 2000, 0.0, "CA", true))
+//      .toDF("loan_id", "funded_amnt", "paid_amnt", "addr_state", "closed")
+//
+//    loanUpdates.write
+//      .format("delta")
+//      .mode("append")
+//      .option("mergeSchema", "true")
+//      .save(deltaPath)
+//
+//    // Se pueden realizar operaciones SQL directamente
+//
     val deltaTable = DeltaTable.forPath(spark, deltaPath)
+//
+//    deltaTable.update(
+//      col("addr_state") === "OR",
+//        Map("addr_state" -> lit("WA"))
+//    )
+//
+//    spark.sql("SELECT * FROM loans_delta").show()
+//
+//    deltaTable.delete("funded_amnt >= paid_amnt")
+//
+//    spark.sql("SELECT * FROM loans_delta").show()
+//
+//    deltaTable
+//      .alias("t")
+//      .merge(loanUpdates.alias("s"), "t.load_id = s.load_id")
+//      .whenMatched.updateAll()
+//      .whenNotMatched.insertAll()
+//      .execute()
+//
+//    spark.sql("SELECT * FROM loans_delta").show()
 
-    deltaTable.update(
-      col("addr_state") === "OR",
-        Map("addr_state" -> lit("WA"))
-    )
+    // Auditoria de cambios
 
-    spark.sql("SELECT * FROM loans_delta").show()
-
-    deltaTable.delete("funded_amnt >= paid_amnt")
-
-    spark.sql("SELECT * FROM loans_delta").show()
+    deltaTable.history().show()
 
     deltaTable
-      .alias("t")
-      .merge(loanUpdates.alias("s"), "t.load_id = s.load_id")
-      .whenMatched.updateAll()
-      .whenNotMatched.insertAll()
-      .execute()
+      .history(3)
+      .select("version", "timestamp", "operation", "operationParameters")
+      .show(false)
 
-    spark.sql("SELECT * FROM loans_delta").show()
+    // Previous snapshots
+
+    spark.read
+      .format("delta")
+      .option("timestampAsOf", "2025-03-04")
+      .load(deltaPath)
+
+    spark.read
+      .format("delta")
+      .option("versionAsOf", "5")
+      .load(deltaPath)
   }
 }
